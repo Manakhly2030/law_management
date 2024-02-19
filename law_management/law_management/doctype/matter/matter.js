c723e4fd5a778f859b839181385ca26ff3a9114b
peacebrowne <emmanuelbrowne751999@gmail.com>
2024-02-19 16:23:22 +0000

    Completed invoice request

--- law_management/law_management/doctype/matter/matter.js
+++ law_management/law_management/doctype/matter/matter.js
@@ -2,32 +2,27 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Matter", {
-  refresh: async function (frm, cdt, cdn) {
-    const state = frm.doc.workflow_state;
+  refresh: async function (frm) {
+    const { workflow_state, matter_type } = frm.doc;
 
-    const type = frm.doc.matter_type;
-    frm.toggle_display("category", type === "Litigation");
+    frm.toggle_display("category", matter_type === "Litigation");
     frm.set_df_property("category", "reqd", true);
 
-    if (state !== "Draft") {
+    if (workflow_state !== "Draft") {
       frm.toggle_display("amount", true);
-      frm.toggle_display("request_to", true);
-      frm.set_df_property("amount", "reqd", true);
-      frm.set_df_property("request_to", "reqd", true);
+      frm.set_df_property("amoutnt", "reqd", true);
 
       const is_request_sent = await frappe.call({
         method:
           "law_management.law_management.doctype.matter.matter.email_sent",
         args: {
-          docname: frm.docname,
+          docname: frm.doc.name,
         },
       });
 
-      if (is_request_sent.message) {
-        view_invoice_request(frm);
-      } else {
-        toggle_invoice_request(frm);
-      }
+      is_request_sent.message
+        ? view_invoice_request(frm)
+        : toggle_invoice_request(frm);
     }
   },
 
@@ -85,44 +80,49 @@ frappe.ui.form.on("Matter", {
 });
 
 const view_invoice_request = (frm) => {
+  const invoiceRequest = frm.doc.invoice_request;
   frm.remove_custom_button("Send Invoice Request");
+
   frm.add_custom_button(__("View Invoice Request"), () => {
-    frappe.set_route("Form", "Matter Invoice Request", frm.doc.invoice_request);
+    frappe.set_route("Form", "Matter Invoice Request", invoiceRequest);
   });
 };
 
 const toggle_invoice_request = (frm) => {
-  frm.add_custom_button(__("Send Invoice Request"), () => {
-    const amount = findElement('[data-fieldname="amount"]').querySelector(
-      "input"
-    ).value;
-    const request_to = findElement(
-      '[data-fieldname="request_to"]'
-    ).querySelector("input").value;
+  const validate_inputs = () => {
+    const amountField = frm.fields_dict.amount.$input["0"];
+    const descriptionField = frm.fields_dict.description.$input["0"];
+
+    const amount = amountField.value;
+    const description = descriptionField.innerText;
 
     if (amount == 0) {
       frappe.throw("Amount should be above 0!");
-    } else if (!request_to) {
-      frappe.throw("Please select the use to send the invoice request to!");
+    } else if (!description) {
+      frappe.throw("Please enter the description!");
     }
+  };
 
-    frappe.call({
+  const send_invoice_request = async () => {
+    const response = await frappe.call({
       method:
-        "law_management.law_management.doctype.matter.matter.open_email_composer",
+        "law_management.law_management.doctype.matter.matter.send_email_request",
       args: {
-        docname: frm.docname,
+        docname: frm.doc.name,
         amount: frm.doc.amount,
         description: frm.doc.description,
         client_name: frm.doc.client_name,
-        request_to: frm.doc.request_to,
-      },
-      callback: (response) => {
-        frm.set_value("invoice_request", response.message);
-        frappe.msgprint("Email sent successfully!");
-        view_invoice_request(frm);
       },
     });
+
+    frm.set_value("invoice_request", response.message);
+    frm.save();
+    setTimeout(() => frappe.msgprint("Email sent succesfully"), 2000);
+    view_invoice_request(frm);
+  };
+
+  frm.add_custom_button(__("Send Invoice Request"), () => {
+    validate_inputs();
+    send_invoice_request();
   });
 };
-
-const findElement = (selector) => document.querySelector(selector);
